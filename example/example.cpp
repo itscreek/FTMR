5b0aed97a1e413741fa9dc2b08441bcc1ea3b6cd
Itsuki Urakawa <urakawa.itsuki.78z@st.kyoto-u.ac.jp>
2024-11-19 13:40:48 +0900

    Update

--- example/example.cpp
+++ example/example.cpp
@@ -5,8 +5,9 @@
 
 int main() {
     const std::vector<std::pair<int, int>> edges = {
-        {0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}, {2, 6}, {7, 3}};
-    FTMR::MultitreeRecolorability recolorability(edges, 8);
+        {0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5},
+        {5, 6}, {7, 2}, {2, 8}, {3, 9}, {10, 4}};
+    FTMR::MultitreeRecolorability recolorability(edges, 11);
 
     bool condition_s = recolorability.CheckConditionS();
     bool condition_cp = recolorability.CheckConditionCP();
@@ -18,4 +19,7 @@ int main() {
               << std::endl;
     std::cout << "Condition CV: " << (condition_cv ? "True" : "False")
               << std::endl;
+
+    recolorability.OutputInfoOfPathRelationGraph(
+        "polytree_/not_ncc2/v11_2_path.dot", false);
 }
